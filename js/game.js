f0a51179ec31bc505e010b66c7b1f16227d69586
Tim Lehming <timlehming@aol.com>
2026-07-31 15:42:45 +0200

    Add basic gameplay state handling and START button event listener to change to gameplay state

--- js/game.js
+++ js/game.js
@@ -1,7 +1,70 @@
+// Game Screens
+const gameStartScreenNode = document.querySelector("#game-start-screen");
+const gameplayScreenNode = document.querySelector("#gameplay-screen");
+const gameOverScreenNode = document.querySelector("#game-over-screen");
+const gameWinScreenNode = document.querySelector("#game-win-screen");
+
+// Buttons
+const startButtonNode = gameStartScreenNode.querySelector("#start-game-btn");
+const restartButtonOnGameOverNode =
+  gameOverScreenNode.querySelector("#restart-game-btn");
+const restartButtonOnWinNode =
+  gameWinScreenNode.querySelector("#restart-game-btn");
+
 class Game {
   constructor() {
-    console.log("ctor called");
+    this.changeGameState("start");
+  }
+
+  changeGameState(state) {
+    switch (state) {
+      case "start":
+        this.showGameStartScreen();
+        break;
+      case "gameplay":
+        this.showGameplayScreen();
+        break;
+      case "gameover":
+        this.showGameOverScreen();
+        break;
+      case "gamewin":
+        this.showGameWinScreen();
+        break;
+    }
+  }
+
+  showGameStartScreen() {
+    gameplayScreenNode.style.display = "none";
+    gameOverScreenNode.style.display = "none";
+    gameWinScreenNode.style.display = "none";
+    gameStartScreenNode.style.display = "flex";
+  }
+
+  showGameplayScreen() {
+    gameStartScreenNode.style.display = "none";
+    gameOverScreenNode.style.display = "none";
+    gameWinScreenNode.style.display = "none";
+    gameplayScreenNode.style.display = "flex";
+  }
+
+  showGameOverScreen() {
+    gameStartScreenNode.style.display = "none";
+    gameplayScreenNode.style.display = "none";
+    gameWinScreenNode.style.display = "none";
+    gameOverScreenNode.style.display = "flex";
+  }
+
+  showGameWinScreen() {
+    gameStartScreenNode.style.display = "none";
+    gameplayScreenNode.style.display = "none";
+    gameOverScreenNode.style.display = "none";
+    gameWinScreenNode.style.display = "flex";
   }
 }
 
-new Game();
+// Init new game on load
+let game = new Game();
+
+startButtonNode.addEventListener("click", () => {
+  game.changeGameState("gameplay");
+});
